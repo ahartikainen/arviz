c3b9e53e5c5c9a2e252074a506cd48f2911c7336
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2018-12-15 03:02:55 +0200

    Update plot_posterior.py

--- examples/plot_posterior.py
+++ examples/plot_posterior.py
@@ -11,5 +11,5 @@ az.style.use('arviz-darkgrid')
 data = az.load_arviz_data('centered_eight')
 
 coords = {'school': ['Choate']}
-az.plot_posterior(data, var_names=['mu', 'theta'], coords=coords, rope=(-1, 1))
+#az.plot_posterior(data, var_names=['mu', 'theta'], coords=coords, rope=(-1, 1))
 
