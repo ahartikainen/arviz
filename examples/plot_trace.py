ce9bac79b10543f0bf3e2724cdbf1a657c40367a
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2018-12-15 02:39:41 +0200

    Update plot_trace.py

--- examples/plot_trace.py
+++ examples/plot_trace.py
@@ -8,5 +8,5 @@ import arviz as az
 
 az.style.use('arviz-darkgrid')
 
-data = az.load_arviz_data('non_centered_eight')
-az.plot_trace(data, var_names=('tau', 'mu'))
+#data = az.load_arviz_data('non_centered_eight')
+#az.plot_trace(data, var_names=('tau', 'mu'))
